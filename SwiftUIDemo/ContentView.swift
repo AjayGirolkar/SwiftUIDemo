85df72253423322a542af5b9a9a7c2e150796091
Ajay Girolkar <ajaygirolkar@gmail.com>
2021-06-17 11:13:44 +0530

    Swift learning demo

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Ajay Girolkar on 16/06/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(menu) { section in
+                    Section(header: Text(section.name)) {
+                        ForEach(section.items) { item in
+                            NavigationLink(destination: Text(item.name)) {
+                                ItemRow(item: item)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Navigation Title")
+            .listStyle(GroupedListStyle())
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
